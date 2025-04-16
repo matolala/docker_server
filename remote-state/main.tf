2fa8e5958240eeea80473af9aa8c22aa8478326d
matolala <martinsadagbusi@yahoo.com>
2025-04-16 16:42:35 -0500

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
-  profile= "yusuf"
+  region = "us-east-2"
+  profile= "martins"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yusuf-docker-tfstate"
+  bucket = "martins-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
